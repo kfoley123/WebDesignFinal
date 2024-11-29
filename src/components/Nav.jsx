8a69a822e820018273bc4075c0854e22993375f1
Kortney <kortney_foley@hotmail.com>
2024-11-29 18:34:02 -0400

    installed tailwind and tested some dummy styles

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,7 +1,10 @@
 export default function Nav(){
     return (
-        <>
-        <p>this is the nav</p>
-        </>
+        <nav>
+            <div>
+                
+            </div>
+
+        </nav>
     )
 }
