f4ecb6dd0f5a0fa17791030a01c6225f5cceb9b4
Kortney <kortney_foley@hotmail.com>
2024-12-05 14:03:48 -0400

    added and styled card component and quickshop component that renders instances of card

---
+++ src/components/Card.jsx
@@ -0,0 +1,20 @@
+export default function Card({ name, image }) {
+    return (
+        <div className="flex flex-col items-center justify-center border-2 border-black p-2 my-5 mx-3 w-40  lg:w-60">
+        <img 
+            src={image} 
+            alt={name} 
+            className="mb-2 w-full max-h-[50%] object-contain" 
+        /> 
+        <h1 className="text-lg font-semibold mb-1 text-center">{name}</h1> 
+        <span className="mb-2 text-center">QTY: 1</span> 
+        <a 
+            href="#"
+            className="bg-customPink text-white font-semibold py-1 px-4 rounded-full shadow-lg hover:bg-pink-600 text-center"
+        >
+            Add to Cart
+        </a>
+    </div>
+    
+    );
+}
--- src/components/QuickShop.jsx
+++ src/components/QuickShop.jsx
@@ -1,7 +1,27 @@
-export default function QuickShop(){
-    return(
-        <>
-        Quickshop Section!!
-        </>
-    )
+import Card from "./Card";
+
+export default function QuickShop() {
+  const inStockItems = [
+    { name: "Red Velvet Raspberry Cupcake", image: "/images/redvelvetraspberry.webp", id: 1 },
+    { name: "Lemon Drop Deluxe Cupcake", image: "/images/lemon.webp", id: 2 },
+    { name: "Chocolate Oreo Crumble Cupcake", image: "/images/chocolateoreo.webp", id: 3 },
+    { name: "Hot Fudge Sundae Cupcake", image: "/images/hotfudgesundae.webp", id: 4 }
+  ];
+
+  return (
+    <div className="bg-white flex flex-col items-center justify-center">
+      <a
+        href="#"
+        className="bg-pink-500 mt-12 text-white font-semibold text-2xl px-12 py-6 md:text-2xl md:px-18 md:py-6 rounded-lg shadow-md hover:bg-pink-600"
+      >
+        SHOP ALWAYS AVAILABLE
+      </a>
+    
+      <div className="grid grid-cols-2 md:grid-cols-4 gap-4 mt-8">
+        {inStockItems.map(item => (
+          <Card key={item.id} name={item.name} image={item.image} />
+        ))}
+      </div>
+    </div>
+  );
 }
