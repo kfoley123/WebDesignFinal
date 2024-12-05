7c91ac049301d27ecebec87c4967074f72fad14b
Kortney <kortney_foley@hotmail.com>
2024-12-04 23:09:02 -0400

    removed default vite styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import './App.css'
 import Nav from './components/Nav';
+import Footer from './components/Footer';
 
 export default function App() {
   return (
     <>
-     <Nav/>
+     {/* <Nav/> */}
+     <Footer/>
     </>
  
   );
